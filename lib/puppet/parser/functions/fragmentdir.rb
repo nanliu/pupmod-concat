f9d0edf97c0321224d3cfe19ae1e5f2df8dd70d1
Nan Liu <nan.liu@gmail.com>
2011-05-19 20:01:31 -0700

    This should use vardir instead of fixed path. (However this is server side, and if the client have a different configuration this won't cover that scenario.)

--- lib/puppet/parser/functions/fragmentdir.rb
+++ lib/puppet/parser/functions/fragmentdir.rb
@@ -18,6 +18,6 @@
 #
 module Puppet::Parser::Functions
     newfunction(:fragmentdir, :type => :rvalue, :doc => "Returns the fragment directory for a given concat build.") do |args|
-        "/var/lib/puppet/concat/fragments/#{args}"
+        "#{Puppet[:vardir]}/concat/fragments/#{args}"
     end
 end
